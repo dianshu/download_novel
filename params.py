ab885dda8640b57eaaff6dcfdc26162f165249f3
dianshu <feishicheng2015@163.com>
2018-04-02 14:16:13 +0800

    add comment

--- params.py
+++ params.py
@@ -1,3 +1,6 @@
+# 目录url
 catagory_url = 'http://www.80txt.com/txtml_59445.html'
+# 请求头
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'}
+# 并行的进程数
 process_num = 8
